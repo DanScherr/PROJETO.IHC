b08ef1db440cb49b7185c9cd2586173e414aaff1
FabianoPretoJr <fabiano.pretojr@hotmail.com>
2023-11-26 13:30:36 -0300

    endpoint de Habilidade

--- PROJETO.IHC.API/Controllers/HabilidadeController.cs
+++ PROJETO.IHC.API/Controllers/HabilidadeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PROJETO.IHC.Domain.DTOs.Habilidade;
 using PROJETO.IHC.Domain.Interfaces.Services;
+using System.Net;
 
 namespace PROJETO.IHC.API.Controllers
 {
@@ -14,6 +16,81 @@ namespace PROJETO.IHC.API.Controllers
         public HabilidadeController(IHabilidadeService habilidadeService)
         {
             _habilidadeService = habilidadeService;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetHabilidadesById(int id)
+        {
+            try
+            {
+                var habilidade = _habilidadeService.GetHabilidadeById(id);
+
+                return Ok(habilidade);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllHabilidades()
+        {
+            try
+            {
+                var listHabilidades = _habilidadeService.GetAllHabilidades();
+
+                return Ok(listHabilidades);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SaveHabilidade([FromBody] HabilidadeInsertDTO habilidadeInsertDTO)
+        {
+            try
+            {
+                var habilidadeOutputDTO = _habilidadeService.InsertHabilidade(habilidadeInsertDTO);
+
+                return Ok(habilidadeOutputDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateHabilidade([FromBody] HabilidadeUpdateDTO habilidadeUpdateDTO)
+        {
+            try
+            {
+                var habilidadeOutputDTO = _habilidadeService.UpdateHabilidade(habilidadeUpdateDTO);
+
+                return Ok(habilidadeOutputDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteHabilidade(int id)
+        {
+            try
+            {
+                var isDelete = _habilidadeService.DeleteHabilidade(id);
+
+                return Ok(isDelete);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
         }
     }
 }
