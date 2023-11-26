d584249c0f1d361576aeb0a144bacad33b6d4860
FabianoPretoJr <fabiano.pretojr@hotmail.com>
2023-11-26 13:24:58 -0300

    endpoint de empresa

--- PROJETO.IHC.API/Controllers/EmpresaController.cs
+++ PROJETO.IHC.API/Controllers/EmpresaController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PROJETO.IHC.Domain.DTOs.Empresa;
 using PROJETO.IHC.Domain.Interfaces.Services;
+using System.Net;
 
 namespace PROJETO.IHC.API.Controllers
 {
@@ -14,6 +16,81 @@ namespace PROJETO.IHC.API.Controllers
         public EmpresaController(IEmpresaService empresaService)
         {
             _empresaService = empresaService;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetEmpresasById(int id)
+        {
+            try
+            {
+                var empresa = _empresaService.GetEmpresaById(id);
+
+                return Ok(empresa);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmpresas()
+        {
+            try
+            {
+                var listEmpresas = _empresaService.GetAllEmpresas();
+
+                return Ok(listEmpresas);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SaveEmpresa([FromBody] EmpresaInsertDTO empresaInsertDTO)
+        {
+            try
+            {
+                var empresaOutputDTO = _empresaService.InsertEmpresa(empresaInsertDTO);
+
+                return Ok(empresaInsertDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmpresa([FromBody] EmpresaUpdateDTO empresaUpdateDTO)
+        {
+            try
+            {
+                var empresaOutputDTO = _empresaService.UpdateEmpresa(empresaUpdateDTO);
+
+                return Ok(empresaOutputDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteEmpresa(int id)
+        {
+            try
+            {
+                var isDelete = _empresaService.DeleteEmpresa(id);
+
+                return Ok(isDelete);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
         }
     }
 }
