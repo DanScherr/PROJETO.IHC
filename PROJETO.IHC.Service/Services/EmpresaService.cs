a2aa854c250818d674854ccddc6c7935b71fb1dc
FabianoPretoJr <fabiano.pretojr@hotmail.com>
2023-11-29 07:35:36 -0300

    serviços de empresa e correção de docker

--- PROJETO.IHC.Service/Services/EmpresaService.cs
+++ PROJETO.IHC.Service/Services/EmpresaService.cs
@@ -1,4 +1,5 @@
-﻿using PROJETO.IHC.Domain.DTOs.Empresa;
+﻿using PROJETO.IHC.Domain.DTOs.Colaborador;
+using PROJETO.IHC.Domain.DTOs.Empresa;
 using PROJETO.IHC.Domain.Entities;
 using PROJETO.IHC.Domain.Interfaces.Repositories;
 using PROJETO.IHC.Domain.Interfaces.Services;
@@ -69,17 +70,97 @@ namespace PROJETO.IHC.Service.Services
 
         public EmpresaOutputDTO InsertEmpresa(EmpresaInsertDTO empresaInsertDTO)
         {
-            throw new NotImplementedException();
+            var empresa = new Empresa()
+            {
+                NomeEmpresa = empresaInsertDTO.NomeEmpresa,
+                DocumentoCNPJ = empresaInsertDTO.DocumentoCNPJ,
+                SiteEmpresa = empresaInsertDTO.SiteEmpresa,
+                Email = empresaInsertDTO.Email,
+                Telefone = empresaInsertDTO.Telefone,
+                Logradouro = empresaInsertDTO.Logradouro,
+                Numero = empresaInsertDTO.Numero,
+                Bairro = empresaInsertDTO.Bairro,
+                Cidade = empresaInsertDTO.Cidade,
+                UF = empresaInsertDTO.UF,
+                CEP = empresaInsertDTO.CEP
+            };
+
+            empresa.Ativar();
+
+            _empresaRepository.Inserir(empresa);
+
+            if (empresa.Id == 0)
+                throw new NullReferenceException("Falha ao inserir Empresa");
+
+            return new EmpresaOutputDTO()
+            {
+                Id = empresa.Id,
+                NomeEmpresa = empresa.NomeEmpresa,
+                DocumentoCNPJ = empresa.DocumentoCNPJ,
+                SiteEmpresa = empresa.SiteEmpresa,
+                Email = empresa.Email,
+                Telefone = empresa.Telefone,
+                Logradouro = empresa.Logradouro,
+                Numero = empresa.Numero,
+                Bairro = empresa.Bairro,
+                Cidade = empresa.Cidade,
+                UF = empresa.UF,
+                CEP = empresa.CEP,
+                Ativo = empresa.Ativo
+            };
         }
 
         public EmpresaOutputDTO UpdateEmpresa(EmpresaUpdateDTO empresaUpdateDTO)
         {
-            throw new NotImplementedException();
+            this.GetEmpresaById(empresaUpdateDTO.Id);
+
+            var empresa = new Empresa()
+            {
+                Id= empresaUpdateDTO.Id,
+                NomeEmpresa = empresaUpdateDTO.NomeEmpresa,
+                DocumentoCNPJ = empresaUpdateDTO.DocumentoCNPJ,
+                SiteEmpresa = empresaUpdateDTO.SiteEmpresa,
+                Email = empresaUpdateDTO.Email,
+                Telefone = empresaUpdateDTO.Telefone,
+                Logradouro = empresaUpdateDTO.Logradouro,
+                Numero = empresaUpdateDTO.Numero,
+                Bairro = empresaUpdateDTO.Bairro,
+                Cidade = empresaUpdateDTO.Cidade,
+                UF = empresaUpdateDTO.UF,
+                CEP = empresaUpdateDTO.CEP
+            };
+
+            _empresaRepository.Alterar(empresa);
+
+            return new EmpresaOutputDTO()
+            {
+                Id = empresa.Id,
+                NomeEmpresa = empresa.NomeEmpresa,
+                DocumentoCNPJ = empresa.DocumentoCNPJ,
+                SiteEmpresa = empresa.SiteEmpresa,
+                Email = empresa.Email,
+                Telefone = empresa.Telefone,
+                Logradouro = empresa.Logradouro,
+                Numero = empresa.Numero,
+                Bairro = empresa.Bairro,
+                Cidade = empresa.Cidade,
+                UF = empresa.UF,
+                CEP = empresa.CEP,
+                Ativo = empresa.Ativo
+            };
         }
 
         public bool DeleteEmpresa(int id)
         {
-            throw new NotImplementedException();
+            if (id < 1)
+                throw new ArgumentException($"Id: {id} está inválido");
+
+            var isDelete = _empresaRepository.Deletar(id);
+
+            if (!isDelete)
+                throw new KeyNotFoundException($"Empresa com Id: {id} não encontrada");
+
+            return isDelete;
         }
     }
 }
