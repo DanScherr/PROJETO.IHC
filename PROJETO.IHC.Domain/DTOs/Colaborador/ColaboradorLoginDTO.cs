4588f25a3418b47874473f8d55bc086b3520afae
FabianoPretoJr <fabiano.pretojr@hotmail.com>
2023-11-29 21:32:10 -0300

    Ajustes DTO

---
+++ PROJETO.IHC.Domain/DTOs/Colaborador/ColaboradorLoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PROJETO.IHC.Domain.DTOs.Colaborador
+{
+    public class ColaboradorLoginDTO
+    {
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
